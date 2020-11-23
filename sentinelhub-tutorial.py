8070f8c9e2a80bee80aeb02459e16861977dcdc0
xaviermiles <74747457+xaviermiles@users.noreply.github.com>
2020-11-23 20:22:03 +1300

    Update sentinelhub-tutorial.py

--- sentinelhub-tutorial.py
+++ sentinelhub-tutorial.py
@@ -1,12 +1,4 @@
-import os
-import datetime
-import numpy as np
-import matplotlib.plot as plt
-
 from sentinelhub import SHConfig
-from sentinelhub import MimeType, CRS, BBox, SentinelHubRequest, \
-    SentinelHubDownloadClient, DataCollection, bbox_to_dimensions, DownloadRequest
-from utils import plot_image
 
 # Setup Sentinelhub API interaction
 CLIENT_ID = 'db3d37b7-38e9-44e3-a4e5-00c0e49bdede'
@@ -17,9 +9,19 @@ config = SHConfig()
 if CLIENT_ID and CLIENT_SECRET:
     config.sh_client_id = CLIENT_ID
     config.sh_client_secret = CLIENT_SECRET
-
+    
 if config.sh_client_id == '' or config.sh_client_secret == '':
     print("LOL Warning! To use Sentinel Hub services, please provide the credentials (client ID and client secret).")
+
+
+import os
+import datetime
+import numpy as np
+import matplotlib.plot as plt
+
+from sentinelhub import MimeType, CRS, BBox, SentinelHubRequest, \
+    SentinelHubDownloadClient, DataCollection, bbox_to_dimensions, DownloadRequest
+from utils import plot_image
 
 betsiboka_coords_wgs84 = [46.16, -16.15, 46.51, -15.58]
 resolution = 60
